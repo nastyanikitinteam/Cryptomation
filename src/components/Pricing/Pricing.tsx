c6d6f504eaefba480b527d849abbeb88f39bac48
nastyanikitinteam <nastya@nikitinteam.com>
2022-06-29 16:03:10 +0200

    edit scroll

--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -5,15 +5,20 @@ import styles from './pricing.module.scss';
 
 const Pricing = () => {
   return (
-    <div className={cn('section', styles.section)} id="pricing">
-      <div className="wrapper">
-        <h2 className="title white">Pricing</h2>
-        <p className={styles.text}>
-          The best choice for developers, hobbyists & web-scale blockchain apps.
-        </p>
-        <Blocks />
+    <>
+      <div className={cn('section', styles.section)}>
+        <div className={styles.anchor} id="pricing"></div>
+
+        <div className="wrapper">
+          <h2 className="title white">Pricing</h2>
+          <p className={styles.text}>
+            The best choice for developers, hobbyists & web-scale blockchain
+            apps.
+          </p>
+          <Blocks />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
